fcdad35286cbb03defe22ec41c9f5080e7e60d7b
Tshiamo <tkomane@protonmail.com>
2017-06-21 19:39:38 +0200

    Added metadata extraction script and added more detail to README file

---
+++ extractor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+A Python script that extracts metadata from jpg images.
+This includes things like location, camera-make, date-taken etc.
+
+By Tshiamo Komane
+"""
+
+from PIL import Image  # makes use of the pillow library
+from PIL.ExifTags import TAGS
+
+image = Image.open("image2.jpg")
+info = image._getexif()  # creates a dictionary with tag-value exif data
+
+for tag, value in info.items():
+    key = TAGS.get(tag, tag)
+    print(key + " " + str(value))  # outputs Exif data
+
+print("#################################################################")
